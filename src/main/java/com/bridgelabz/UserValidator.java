4a90d597f9a90f8f75b8a67a257d4599c35ffa1f
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-17 16:11:51 +0530

    ADDED: Method To Validate First Name

--- src/main/java/com/bridgelabz/UserValidator.java
+++ src/main/java/com/bridgelabz/UserValidator.java
@@ -4,4 +4,8 @@ public class UserValidator {
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration");
     }
+String NAME_REGEX="^[A-Z]{1}[A-Za-z]{2,}$";
+    public boolean validateFirstName(String firstName) {
+        return firstName.matches(NAME_REGEX);
+    }
 }
