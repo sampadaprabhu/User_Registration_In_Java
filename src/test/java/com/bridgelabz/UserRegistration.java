6d3372c79c2a1486a426d92f105c15e6a460c6f6
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-02-17 16:11:51 +0530

    ADDED: Method To Validate First Name

---
+++ src/test/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserRegistration {
+    @Test
+   public void givenFirstName_WhenProper_ShouldReturnTrue() {
+       UserValidator validator=new UserValidator();
+       boolean result=validator.validateFirstName("Sampada");
+       Assert.assertEquals(true,result);
+   }
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnFalse(){
+        UserValidator validator=new UserValidator();
+        boolean result=validator.validateFirstName("sampada");
+        Assert.assertEquals(false,result);
+    }
+
+}
